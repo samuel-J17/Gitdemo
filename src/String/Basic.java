c496d137b034477d0e99d4b5af128fee1ca50099
Leo <leobenjamine021@gmail.com>
2025-05-10 12:45:14 +0530

    Commit from my system

--- src/String/Basic.java
+++ src/String/Basic.java
@@ -9,6 +9,12 @@ public class Basic {
 		System.out.println("leobenjamine001");
 		System.out.println("leobenjamine002");
 		System.out.println("leobenjamine003");
+		
+		System.out.println("leobenjamine0031");
+		
+		System.out.println("leobenjamine0024");
+		System.out.println("leobenjamine0063");
+		System.out.println("leobenjamine0033");
 	}
 
 }
